77e77df307c6dc87452d943d07a8e0f1937c340b
Eduardo Agostini <edu.agostini@gmail.com>
2017-09-26 16:34:07 +1300

    Helpers
    Adding some filters and functions.

--- src/Extension/ParsedownExtension.php
+++ src/Extension/ParsedownExtension.php
@@ -8,7 +8,33 @@ use Parsedown\TokenParser\ParsedownTokenParser;
  * Class ParsedownExtension
  * @package Parsedown\Extension
  */
-class ParsedownExtension extends \Twig_Extension {
+class ParsedownExtension extends \Twig_Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('parsedown', 'parsedown', [
+                'is_safe' => ['all']
+            ]),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('parsedown', 'parsedown', [
+                'is_safe' => ['all']
+            ]),
+        ];
+    }
+
+
     /**
      * {@inheritdoc}
      */
